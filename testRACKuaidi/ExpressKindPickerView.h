016605d45370ce48f2d47b658f6791443295d7e0
吴迪 <wudi@wudideMacBook-Pro.local>
2016-12-05 23:36:45 +0800

    添加了动态计算高度，优化了物流详情页面，增加了‘中通，圆通，申通，汇通，韵达’等快件的查询支持

---
+++ testRACKuaidi/ExpressKindPickerView.h
@@ -0,0 +1,13 @@
+//
+//  ExpressKindPickerView.h
+//  testRACKuaidi
+//
+//  Created by 吴迪 on 16/12/5.
+//  Copyright © 2016年 com.tb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpressKindPickerView : UIPickerView
+
+@end
---
+++ testRACKuaidi/ExpressKindPickerView.m
@@ -0,0 +1,21 @@
+//
+//  ExpressKindPickerView.m
+//  testRACKuaidi
+//
+//  Created by 吴迪 on 16/12/5.
+//  Copyright © 2016年 com.tb. All rights reserved.
+//
+
+#import "ExpressKindPickerView.h"
+
+@implementation ExpressKindPickerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
